d1c241403b4df96de91f6eb12df12bc5ed5d8758
K HARI KRISHNA <88489760+harikrishna7696@users.noreply.github.com>
2023-08-21 14:24:14 +0530

    Add files via upload

--- check_logs.py
+++ check_logs.py
@@ -6,17 +6,28 @@ import os
 from datetime import datetime
 
 
-def get_logs_from_gz(path, usecase, camera_ip, start_time, end_time):
+def get_logs_from_gz(path, usecase, camera_ip, start_time, end_time, cl_name, check_cl):
+    """
+    describe function
+    :param path:
+    :param usecase:
+    :param camera_ip:
+    :param start_time:
+    :param end_time:
+    :return:
+    """
+    global counter
+    counter = False
+
     try:
         with gzip.open(path, 'r') as log:
             log_file = log.read()
             logs = log_file.decode('utf-8')
         for i in logs.split('\n')[1:]:
             eve = i.split(":")
-            # print(eve)
             if path.startswith('event_consumer'):
                 if len(eve) > 5:
-                    if eve[5].strip() == 'Received event json is':
+                    if eve[5].strip() == f'[{camera_ip}] [{usecase}] Received event json is':
                         d = i.split('Received event json is')[1][2:].strip()
                         data_dict = ast.literal_eval(d)
                         dt_time = data_dict['datetime'].split('.')[0]
@@ -33,17 +44,18 @@ def get_logs_from_gz(path, usecase, camera_ip, start_time, end_time):
                             log_time = converted_log_datetime.time()
                             s_time = converted_start_time.time()
                             e_time = converted_end_time.time()
-    
+
                             if s_time <= log_time <= e_time:
-                                print(Fore.RED, dt_time, Fore.GREEN, camera_ip, Fore.YELLOW, current_usecase, Fore.BLUE,
+                                print(Fore.RED, dt_time, Fore.GREEN, camera_ip, Fore.YELLOW, current_usecase,
+                                      Fore.WHITE,
                                       inf)
-                            else:
+                            if log_time > e_time:
                                 print(Fore.GREEN,
                                       "******************************** Fetching Done ***********************************")
                                 exit()
             # BRU_BRU_C168_LC0012
             else:
-                if len(eve) > 7:
+                if len(eve) > 6:
                     if eve[5].strip() == f'[{camera_ip}] Received data json is':
                         d = i.split(f'[{camera_ip}] Received data json is')[1][2:].strip()
                         data_dict = ast.literal_eval(d)
@@ -58,17 +70,36 @@ def get_logs_from_gz(path, usecase, camera_ip, start_time, end_time):
                         log_time = converted_log_datetime.time()
                         s_time = converted_start_time.time()
                         e_time = converted_end_time.time()
-    
+                        if check_cl:
+                            li = [cl[0] for cl in inf]
                         if s_time <= log_time <= e_time:
-                            print(Fore.RED, dt_time, Fore.GREEN, camera_ip, Fore.YELLOW, current_usecase, Fore.BLUE,
-                                  inf)
-                        else:
-                            print(Fore.GREEN, "******************************** Fetching Done ***********************************")
+                            if check_cl and cl_name in li:
+                                print(Fore.RED, dt_time, Fore.GREEN, camera_ip, Fore.YELLOW, current_usecase,
+                                      Fore.WHITE,
+                                      inf, end=' ')
+                                counter = True
+                            elif not check_cl:
+                                print(Fore.RED, dt_time, Fore.GREEN, camera_ip, Fore.YELLOW, current_usecase,
+                                      Fore.WHITE,
+                                      inf)
+
+                        if log_time > e_time:
+                            print(Fore.GREEN,
+                                  "******************************** Fetching Done ***********************************")
                             exit()
+                    elif counter and eve[5].strip() == 'Counter':
+                        print(Fore.RED, 'Counter: ', eve[-1])
+                        print()
+                        counter = False
+
+        print(Fore.GREEN, "******************************** Fetching Done ***********************************")
     except Exception as e:
         print(e)
 
-def get_logs_from_log(path, usecase, camera_ip, start_time, end_time):
+
+def get_logs_from_log(path, usecase, camera_ip, start_time, end_time, cl_name, check_cl):
+    global counter
+    counter = False
     try:
         with open(path, 'r') as log:
             log_file = log.readlines()
@@ -76,7 +107,7 @@ def get_logs_from_log(path, usecase, camera_ip, start_time, end_time):
             eve = i.split(":")
             if path.startswith('event_consumer'):
                 if len(eve) > 5:
-                    if eve[5].strip() == 'Received event json is':
+                    if eve[5].strip() == f'[{camera_ip}] [{usecase}] Received event json is':
                         d = i.split('Received event json is')[1][2:].strip()
                         data_dict = ast.literal_eval(d)
                         dt_time = data_dict['datetime'].split('.')[0]
@@ -93,11 +124,11 @@ def get_logs_from_log(path, usecase, camera_ip, start_time, end_time):
                             log_time = converted_log_datetime.time()
                             s_time = converted_start_time.time()
                             e_time = converted_end_time.time()
-    
                             if s_time <= log_time <= e_time:
-                                print(Fore.RED, dt_time, Fore.GREEN, camera_ip, Fore.YELLOW, current_usecase, Fore.BLUE,
+                                print(Fore.RED, dt_time, Fore.GREEN, camera_ip, Fore.YELLOW, current_usecase,
+                                      Fore.WHITE,
                                       inf)
-                            else:
+                            elif log_time > e_time:
                                 print(Fore.GREEN,
                                       "******************************** Fetching Done ***********************************")
                                 exit()
@@ -117,29 +148,49 @@ def get_logs_from_log(path, usecase, camera_ip, start_time, end_time):
                         s_time = converted_start_time.time()
                         e_time = converted_end_time.time()
                         current_usecase = data_dict['current_usecase']
+                        if check_cl:
+                            li = [cl[0] for cl in inf]
                         if s_time <= log_time <= e_time:
-                            print(Fore.RED, dt_time, Fore.GREEN, camera_ip, Fore.YELLOW, current_usecase, Fore.BLUE,
-                                  inf)
-                        else:
+                            if check_cl and cl_name in li:
+                                print(Fore.RED, dt_time, Fore.GREEN, camera_ip, Fore.YELLOW, current_usecase,
+                                      Fore.WHITE,
+                                      inf)
+                                counter = True
+                            elif not check_cl:
+                                print(Fore.RED, dt_time, Fore.GREEN, camera_ip, Fore.YELLOW, current_usecase,
+                                      Fore.WHITE,
+                                      inf)
+                                counter = True
+                        if log_time > e_time:
                             print(Fore.GREEN,
                                   "******************************** Fetching Done ***********************************")
                             exit()
+                    elif counter and eve[5].strip() == 'Counter':
+                        print(Fore.RED, 'Counter: ', eve[-1])
+                        counter = False
+
+        print(Fore.GREEN, "******************************** Fetching Done ***********************************")
+        exit()
     except Exception as e:
         print(e)
 
+
 args = argparse.ArgumentParser()
 args.add_argument('-path', '-p', type=str, required=True, help='Path to .gz log file')
-args.add_argument('-event', '-e', type=str, required=True,
+args.add_argument('-event', '-eve', type=str, default='On_Block',
                   help='Give the event name ex: On_Block, Note: Give event which is in config file')
 args.add_argument('-cam_ip', '-ip', type=str, required=True, help='camera ip [BRU_BRU_*]')
-args.add_argument('-start_time', type=str, default='07:00:00', help='Start time')
-args.add_argument('-end_time', type=str, default='10:00:00', help='End time')
+args.add_argument('-start_time', '-s', type=str, default='00:00:00', help='Start time')
+args.add_argument('-end_time', '-e', type=str, default='23:00:00', help='End time')
+args.add_argument('-class_name', '-c', type=str, default='', help='Give class to fetch the inference')
+args.add_argument('-check_cl', '-cl', default=False, type=str)
+args.add_argument('-counter', default=True, type=bool)
 
 opt = args.parse_args()
-path = opt.path
-if os.path.exists(path):
-    print(Fore.GREEN, 'File found at {}.'.format(path))
-    print(Fore.BLUE, "Log file -> {}".format(path))
+log_path = opt.path
+if os.path.exists(log_path):
+    print(Fore.GREEN, 'File found at {}.'.format(log_path))
+    print(Fore.WHITE, "Log file -> {}".format(log_path))
     confirmation = input('   Continue checking with above log file: (y/n): ')
     if confirmation == 'y' or confirmation == 'yes':
         print(Fore.GREEN, "***************************************************************************")
@@ -147,9 +198,11 @@ if os.path.exists(path):
         print(Fore.YELLOW, "***********************This Script will Check the logs*********************")
         print(Fore.GREEN, "***************************************************************************")
         print(Fore.GREEN, "***************************************************************************")
-        if path.endswith('.log'):
-            get_logs_from_log(path, opt.event, opt.cam_ip, opt.start_time, opt.end_time)
-        elif path.endswith('.gz'):
-            get_logs_from_gz(path, opt.event, opt.cam_ip, opt.start_time, opt.end_time)
+        if log_path.endswith('.log'):
+            get_logs_from_log(log_path, opt.event, opt.cam_ip, opt.start_time, opt.end_time, opt.class_name,
+                              opt.check_cl)
+        elif log_path.endswith('.gz'):
+            get_logs_from_gz(log_path, opt.event, opt.cam_ip, opt.start_time, opt.end_time, opt.class_name,
+                             opt.check_cl)
 else:
-    print(Fore.RED, 'File not found at {}, Please check the path.'.format(path))
+    print(Fore.RED, 'File not found at {}, Please check the path.'.format(log_path))
